94d082818658faf35365a4f2ebcb696c5f1d62c8
Jorge Padron <jorge-padron99@hotmail.com>
2021-12-19 14:13:53 +0000

    New changes

---
+++ client/js/scripts.js
@@ -0,0 +1,19 @@
+/****Validación formulario*****/ 
+
+function validation(event) {
+    var forms = document.querySelectorAll('.needs-validation');
+    
+    Array.prototype.slice.call(forms).forEach( function(form)
+    {
+        form.addEventListener( 'submit', function (event)
+        {
+            if ( !form.checkValidity() )
+            {
+                event.preventDefault()
+                event.stopPropagation();
+            }
+
+            form.classList.add( 'was-validated' );
+      }, false );
+    } );
+}
